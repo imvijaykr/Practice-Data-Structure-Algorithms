9175b1d84fadd2928459af64a1fb156b8c6dee49
Vijay Kumar <vijaymaurymathiya2004@gmail.com>
2026-03-27 23:56:19 +0530

    Missing Elements from Array

---
+++ 05.Array/Missing_elementFromArray.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+// void findMissing(int a[],int n){
+//     for(int i=0;i<n;i++){
+//         int index=abs(a[i]);
+//         if(a[index -1]>0){
+//             a[index-1] *= -1;
+//         }
+
+//     }
+//     for(int i=0;i<n;i++){
+//         if(a[i]>0){
+//             cout<<i+1<<" ";
+//         }
+//     }
+// }
+//MTHD 2 sorting and swapping
+void findMissing(int a[],int n){
+    int i=0;
+    while(i<n){
+        int index = a[i]-1;
+        if(a[i] != a[index]){
+            swap(a[i],a[index]);
+
+        }
+        else{
+         i++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(a[i] != i+1){
+            cout<<i+1<<" ";
+        }
+    }
+    cout<<endl;
+
+}
+int main(){
+int a[]= {1,3,3,5,4};
+int n= sizeof(a)/sizeof(int);
+findMissing(a,n);
+
+    return 0;
+}
